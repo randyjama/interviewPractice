6a20480f71d474b66e9362703765b915cda96d68
Randy Jama <randy.jama@tasktop.com>
2019-08-14 13:02:47 -0700

    local initial setup

---
+++ ch4/solutions.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import structures.*;
+
+public class solutions{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
+
+
+
+
+
+
+
+
+
