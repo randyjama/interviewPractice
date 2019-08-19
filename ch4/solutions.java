fde3a17d613900f46f255cba09135169227fa995
Randy Jama <randy.jama@tasktop.com>
2019-08-19 11:37:01 -0700

    put ctci library in

--- ch4/solutions.java
+++ ch4/solutions.java
@@ -1,11 +1,33 @@
+import CtCILibrary.*;
 import java.util.*;
 import structures.*;
 
 public class solutions{
 
-     public static void main(String []args){
-        System.out.println("Hello World");
-     }
+   static void listOfDepths(TreeNode root) {
+      
+   }
+
+
+   public static void printResult(ArrayList<LinkedList<TreeNode>> result){
+		int depth = 0;
+		for(LinkedList<TreeNode> entry : result) {
+			Iterator<TreeNode> i = entry.listIterator();
+			System.out.print("Link list at depth " + depth + ":");
+			while(i.hasNext()){
+				System.out.print(" " + ((TreeNode)i.next()).data);
+			}
+			System.out.println();
+			depth++;
+		}
+   }
+   
+	public static void main(String[] args) {
+		int[] nodes_flattened = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		TreeNode root = AssortedMethods.createTreeFromArray(nodes_flattened);
+		ArrayList<LinkedList<TreeNode>> list = createLevelLinkedList(root);
+		printResult(list);
+	}
 }
 
 
